5ad3a76a97d15c0d9ba81823caee9579652ab7d5
Brenofk <brenofernandosoaresa.d.s@gmail.com>
2023-04-01 20:59:03 -0300

    Add imagens ao texto, e textos ao codigo.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+
 const mario = document.querySelector('.mario');
 const tubo = document.querySelector('.tubo');
 
@@ -11,11 +12,35 @@ const jump = () => {
 
 const loop = setInterval(() => {
     
+    console.log("loop")
+
     const tuboPosition = tubo.offsetLeft;
-    console.log(tuboPosition)
+    const marioPosition = +window.getComputedStyle(mario).bottom.replace("px","");
+
+    console.log(marioPosition);
+    
+
+   
+    if (tuboPosition <= 120 && tuboPosition > 0 && marioPosition < 80 ) {
+
+        tubo.style.animation = "none";
+        tubo.style.left = "${tuboPosition}px";
+
+        mario.style.animation = "none";
+        mario.style.bottom = "${marioPosition}px";
+        
+        mario.src = " ./images/game-over.png";
+        mario.style.width = "75px"
+        mario.style.marginleft = "50px"
+
+        clearInterval(loop);
+        
 
 
-}, 10)  
+   
+   
+    }
+}, 10)      
 
     document.addEventListener('keydown', jump);
 
